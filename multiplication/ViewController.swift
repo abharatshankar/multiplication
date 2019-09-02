f467fd0997357c6169c059cdb30a9ee6b05551f4
bharat <bharatshankar5>
2019-09-02 22:46:20 +0530

    added ui for first page

--- multiplication/ViewController.swift
+++ multiplication/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.  hi
+        
+//        let gradient = CAGradientLayer()
+//        gradient.frame = myGradView.bounds
+//        gradient.startPoint = CGPoint.zero
+//        gradient.endPoint = CGPoint(x: 1, y: 1)
+//        gradient.colors = [(UIColor(red: 34.0 / 255.0, green: 211 / 255.0, blue: 198 / 255.0, alpha: 1.0)).cgColor, (UIColor(red: 145 / 255.0, green: 72.0 / 255.0, blue: 203 / 255.0, alpha: 1.0)).cgColor].compactMap { $0 }
+//        myGradView.layer.addSublayer(gradient)
+
     }
 
 
